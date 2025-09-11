4f93e9b02cc5065229ffda2bee940254bf67e42d
joejangwoo <nabbe123@naver.com>
2025-09-11 15:47:40 +0900

    search

--- 0메모장.py
+++ 0메모장.py
@@ -1,7 +1,16 @@
-g =input()
-print('입력받은 숫자 : {g}')
+#g =input()
+#print('입력받은 숫자 : {g}')
 
 ' git  = 로컬 ,커밋 ,메인폴더 깃 추가 , 푸시'
+import random
 
+numer = random.randint(1,4)
+num = input()
+print(f'{num}')
 
-
+running = True
+while running:
+    if num == numer:
+        running = False
+    elif num < numer:
+        print(
