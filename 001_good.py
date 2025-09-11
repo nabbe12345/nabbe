36d380e7cc806220d789b70b321a30a937e30f88
jomr <nabbe123@naver.com>
2025-09-11 22:59:49 +0900

    good

--- 001_good.py
+++ 001_good.py
@@ -1,2 +1,22 @@
-a = [1,2,3,4,5,6]
+a = [3,4,1,2,3,4,5,6,1,3,4,2,4]
+start = 0
 for n in a:
+    if n == 4:
+        print(f'4 가 있는 인덱스 : {start}')
+    start += 1
+
+a = [3,4,1,2,3,4,5,6,1,3,4,2,4]
+start = 0
+running = True
+while running:
+    start = a.index(4, start)
+    print(f'4에 값은 {start}에 있다')
+    start += 1
+    if start == len(a):
+        running = False
+
+a = [3,4,1,2,3,4,5,6,1,3,4,2,4]
+
+for i,v in enumerate(a):
+    if v == 4:
+        print(f'4에 값은 {i}입니다!')
