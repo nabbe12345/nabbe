72c35f6252d76876df9e50a168b7d87f7a024a2c
joejangwoo <nabbe123@naver.com>
2025-09-12 13:05:54 +0900

    set 소개

---
+++ 18_set.py
@@ -0,0 +1,8 @@
+# set 은 순서가 없고 중복을 허용하지 않는다
+number_set = set([1,2,3,4,5,6])
+print(number_set)
+
+
+str_set = set('HelloWorld')          # 중복을 제외하고 담는다 ex) L 3개  o 2개
+print(str_set)                       # 그래서 중복 제거용도로 사용된다
+
