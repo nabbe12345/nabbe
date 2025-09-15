18f6567d458619d5bd567a8cd5bad2d2ac8cccbc
joejangwoo <nabbe123@naver.com>
2025-09-15 17:23:02 +0900

    클래스 멤버 객체화 과정

--- advance/04_member.py
+++ advance/04_member.py
@@ -1,10 +1,12 @@
 class Robot:
 
-    # 생성자? 객체화 할때 호출되는 함수의 일종으로
+    # 생성자 : 객체화 할때 호출되는 함수의 일종으로
     # 객체화가 될때 가장 먼저 호출된다
     # ex)Robot()을 호출 할때 init이 호출
 
     def __init__(self):
+        #초기화를 한다 (최초의 값을 주어준다)
+        # ex)강아지 이름 / 목적:우리집을 잘 지켜
         print('Robot 이 복사될때 제일 먼저 호출 되는 멤버')
 
     def doIt(self):
@@ -12,3 +14,5 @@ class Robot:
 
 robot = Robot()
 robot.doIt()
+
+#robot = Robot() 으로 객체화 요청 -> 생성자 호출(init) -> 객체화
