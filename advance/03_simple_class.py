6a88821a5490457d7fd703b600f3e21a9c109984
joejangwoo <nabbe123@naver.com>
2025-09-15 12:46:03 +0900

    클래스 생성 및 사용

---
+++ advance/03_simple_class.py
@@ -0,0 +1,9 @@
+# java 에서는 파일명이 == 클래스명
+# 파이썬에서는 꼭 그렇진 않다
+# 클래스명 앞에는 꼭 대문자
+class Student: # Student 라는 클래스 (학생과 관련된 함수 및 변수가 들어오겠구나 예측 가능)
+  pass # pass 는 함수나 클래스에 아무것도 없을때 오류방지를 위해 넣는 키워드
+
+std1 = Student()     #Student() 함수를 복사해서 std1 담는것
+std2 = Student()
+std3 = Student()
