bd84e8c5e97e142b1a7faf9612aaab3437721a56
joejangwoo <nabbe123@naver.com>
2025-09-15 11:39:46 +0900

    모듈 사용 2

--- advance/01_module_use.py
+++ advance/01_module_use.py
@@ -5,5 +5,8 @@ from oper import sum
 
 print(f'sum() 함수 실행: {sum(5,10)}')
 
-# import 모듈
+# import 모듈          # .은 ~ 밑에       as는 이름을 줄일수있다/별명
+import oper as op
+print(f'minus() 함수 실행: {op.minus(10,5)}')
+
 
