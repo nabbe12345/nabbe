394a7193185122268921cf380e3087eb68e65578
joejangwoo <nabbe123@naver.com>
2025-09-15 14:37:06 +0900

    클래스 초기화

--- advance/05_initial.py
+++ advance/05_initial.py
@@ -6,7 +6,7 @@ class Puppy:
 
     def __init__(self,name,goal): # 생성자 : 객체화시 호출되는 '일단은 함수'
         #self = 나를 의미함
-        # 받아온 name 과 goal 은 이 생성자르 벗어날수 없다.(생성자의 쓰임이 다하면 함께 없어진다.)
+        # 받아온 name 과 goal 은 이 생성자를 벗어날수 없다.(생성자의 쓰임이 다하면 함께 없어진다.)
         # 그래서 클래스(객체) 멤버 에다가 넣어줘야, 객체가 살아있는 동안 사용이 가능 하다.
         # 그런데  name = name 형태로는 어떤것이 객체의 멤버인지 알 수 없다.
         # 그래서 멤버인 녀석은 self 를 이용하여 표시해 준다.
@@ -14,3 +14,4 @@ class Puppy:
         self.goal=goal
 
 puppy = Puppy('멍멍이','집지키기')
+print(f'이름 : {puppy.name} / 목적: {puppy.goal}')
