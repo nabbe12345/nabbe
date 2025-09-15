335a67a053b2f8f201deb228111a7de233e4d80f
joejangwoo <nabbe123@naver.com>
2025-09-15 17:45:32 +0900

    메모장

---
+++ memooooooooooo/Study.py
@@ -0,0 +1,16 @@
+class Study:
+
+    study = ''
+    book = ''
+
+
+    def __init__(self,study,book):
+        self.study = study
+        self.book  = book
+
+
+
+school =Study('파이썬','라이브러리')
+print(f'{school.study},{school.book}')
+
+
