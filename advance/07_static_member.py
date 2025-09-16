b3c7cf44e25a344e0e48178b1ff527b006613949
joejangwoo <nabbe123@naver.com>
2025-09-16 10:55:14 +0900

    정적멤버2

--- advance/07_static_member.py
+++ advance/07_static_member.py
@@ -11,3 +11,17 @@ class Robot:
 
     def std_count(self):
         print(f'클래스 메서드 : {self.count}')
+
+
+r1 = Robot()
+r2 = Robot()     #지금 복사본이 2개 있다
+#서로 다른 복사본이니 count를 건드렸을때 서로 영향 받지 않음
+
+
+
+r1.count += 1
+print(f'r1.count : {r1.count}')
+print(f'r2.count : {r2.count}')
+r2.count = 100
+print(f'r1.count : {r1.count}')
+print(f'r2.count : {r2.count}')
