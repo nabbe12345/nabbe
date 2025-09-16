90b2d5e508e6e4951ee460126d03946bfe1929a0
joejangwoo <nabbe123@naver.com>
2025-09-16 15:36:35 +0900

    대략적인 에러 메시지

--- advance/11_handling.py
+++ advance/11_handling.py
@@ -7,9 +7,11 @@ try:
         idx = num_list.index(3,idx) # ValueError: 3 is not in list
         print(f'3 은 {idx} 인덱스에 있습니다.')
         idx += 1
-except ValueError:
+except ValueError as e:     #에러를 확인하려면 as 변수 : 를 지정해서 프린트
+    print(e) # 예외에 대한 대략적인 정보 출력
     print('더 이상 3을 찾을 수 없습니다')
 finally:
     print('@@@@@@@@@@@@@@@@@@@끝@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
 
 '''들여쓰기 신경쓰기 '''
+
