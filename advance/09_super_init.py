25d30a352935793debc481a11c0abe6913df2870
joejangwoo <nabbe123@naver.com>
2025-09-16 12:46:29 +0900

    생성자 전달

--- advance/09_super_init.py
+++ advance/09_super_init.py
@@ -8,9 +8,35 @@ class Parent():
 class Child(Parent):
     def __init__(self):
         super().__init__()  # 생략된 부모 생성자
+        # super()는 부모를 뜻함
+        #자식이 부모생성자를 먼저 부름
         print('자식 생성자')
 
 
 
 
 ch = Child()
+
+
+
+# 부모가 초기화가 필요 하다면 자식에게 값을 전달해서 자식이 부모에게 전달하도록 한다
+class SchoolMember:
+
+    name =''
+    age  =0
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+class Teacher(SchoolMember):
+    salary = 0
+
+    def __init__(self,name,age,salary):
+        spuer().__init__(name,age)
+        self.salary = salary
+
+
+
+
+    pass
